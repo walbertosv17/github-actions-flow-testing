eb21ae93681ce8ee69f4520519451c9bf07e01ad
Will <william.mendez@cuemby.com>
2023-08-22 20:47:29 -0600

    refactor(github-actions): consolidate and optimize workflows and scripts to best practices
    - Streamlined commit checks by refactoring into `.github/actions/commit-checks/action.yaml`
    - Removed individual workflow files in favor of a unified `commit-best-practices.yaml` and `protect-branches.yaml`
    - Deleted obsolete scripts related to branch and commit checks

--- .github/workflows/scripts/dev-branch-scripts/check-feature-branch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Check if the branch name starts with "feature/"
-if [[ ! "${GITHUB_REF}" =~ ^refs/heads/feature/ ]]; then
-  echo "❌ Error: You can only merge feature branches into dev."
-  exit 1
-fi
